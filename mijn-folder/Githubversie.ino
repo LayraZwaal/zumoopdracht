03b488e99067958f0e18afe1f9c33b44f23eebd9
Ek8fashi <Emirhankorkmazz40@gmail.com>
2026-06-22 19:23:33 +0200

    Update Githubversie.ino

--- mijn-folder/Githubversie.ino
+++ mijn-folder/Githubversie.ino
@@ -1,6 +1,6 @@
 /**
  * @file   Githubversie.ino
- * @brief Dit is het startpunt van onze Zumo32U4 robot. [cite: 1, 2]
+ * @brief Dit is het startpunt van onze Zumo32U4 robot. 
  * @author Groep 2
  */
 
@@ -15,21 +15,21 @@
 WielAandrijving aandrijving; 
 OndergrondScanner scanner;   
 SnelheidsMeter meter;
-// We gooien de onderdelen in de hoofdregelaar zodat die alles kan aansturen [cite: 3]
+// We gooien de onderdelen in de hoofdregelaar zodat die alles kan aansturen 
 RobotRegelaar regelaar(&aandrijving, &scanner, &meter);
 
 /**
- * @brief De setup die Arduino één keer runt bij het opstarten. [cite: 4]
+ * @brief De setup die Arduino één keer runt bij het opstarten. 
  */
 void setup() {
   Serial.begin(9600);
-  // Sensoren even aanzetten en kalibreren op de baan [cite: 5]
+  // Sensoren even aanzetten en kalibreren op de baan 
   scanner.initialiseer();
   scanner.kalibreer();
 }
 
 /**
- * @brief De oneindige loop die de robot aan de gang houdt. [cite: 6]
+ * @brief De oneindige loop die de robot aan de gang houdt. 
  */
 void loop() {
   regelaar.verwerkLoop();
